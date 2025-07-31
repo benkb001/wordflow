6273a725166f58078956580883943f8c3cd4b72a
Benjamin Bradford <benkb01@Kamsleys-MacBook-Pro.local>
2025-07-31 15:36:41 -0400

    Implement Sign Up

--- wordflow/src/app/components/form/form.component.ts
+++ wordflow/src/app/components/form/form.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 @Component({
     selector: 'Form',
     templateUrl: './form.component.html'
 })
-export class FormComponent { }
+export class FormComponent { 
+    @Input() formGroup!: FormGroup; 
+    @Output() formSubmit = new EventEmitter<void>();
+
+    onSubmit(): void {
+        this.formSubmit.emit();
+    }
+}
