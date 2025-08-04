d3ba197d5a5b4010bd851d1be13159eb7a8e2756
Benjamin Bradford <benkb01@Kamsleys-MacBook-Pro.local>
2025-08-04 15:22:45 -0400

    try to trigger action

--- wordflow/src/app/app-routing.module.ts
+++ wordflow/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './components/signUp/signUp.component';
 import { SignInComponent } from './components/signIn/signIn.component'; 
 import { HomeComponent } from './components/home/home.component'; 
-
+//test
 const routes: Routes = [
   { path: '', component: SignUpComponent },
   { path: 'sign-in', component: SignInComponent },
